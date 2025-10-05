b0e4a485d1c7b6d6aa92523b05725f90791d8564
Thiago Ogawa <thiago.ogawa11@gmail.com>
2025-10-05 02:06:32 -0300

    Listing Detail View (2/2)

--- Aconchego/Project/Aconchego/Core/Explore/ExploreView.swift
+++ Aconchego/Project/Aconchego/Core/Explore/ExploreView.swift
@@ -10,22 +10,24 @@ import SwiftUI
 struct ExploreView: View {
     var body: some View {
         NavigationStack {
-            VStack {
+            ScrollView {
                 SearchAndFilterBar()
-                ScrollView {
-                    LazyVStack(spacing: 32) {
-                        ForEach(0 ... 10, id: \.self) { listing in
-                            NavigationLink(value: listing) {
-                                ListingItemView()
-                                    .frame(height: 400)
-                                    .clipShape(RoundedRectangle(cornerRadius: 10))
-                            }
+                
+                LazyVStack(spacing: 32) {
+                    ForEach(0 ... 10, id: \.self) { listing in
+                        NavigationLink(value: listing) {
+                            ListingItemView()
+                                .frame(height: 400)
+                                .clipShape(RoundedRectangle(cornerRadius: 10))
                         }
                     }
                 }
-                .navigationDestination(for: Int.self) { listing in
-                    Text("Listing detail view...")
-                }
+                .padding()
+            }
+            .navigationDestination(for: Int.self) { listing in
+                ListingDetailView()
+                    .navigationBarBackButtonHidden()
+                    .navigationBarHidden(true)
             }
         }
     }
